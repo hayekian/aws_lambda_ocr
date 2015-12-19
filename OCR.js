33155e2b084028ef14b1fb021c3ff0f9522fa638
Jorge <hayekian@gmail.com>
2015-12-18 21:09:30 -0500

    different files for tmp

--- OCR.js
+++ OCR.js
@@ -1,4 +1,3 @@
-
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'] + ":.";
 process.env.LD_LIBRARY_PATH = process.env.LAMBDA_TASK_ROOT + '/bin'+":.";
 process.env.TESSDATA_PREFIX = __dirname;
